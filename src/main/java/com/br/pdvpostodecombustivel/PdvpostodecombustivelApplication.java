21e697d7af63955c9f3d8e629ddb2ef30dc19cb5
rafaecamargo0505-debug <rafaecamargo0505@gmail.com>
2025-09-12 21:33:14 -0300

    att

---
+++ src/main/java/com/br/pdvpostodecombustivel/PdvpostodecombustivelApplication.java
@@ -0,0 +1,13 @@
+package com.br.pdvpostodecombustivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvpostodecombustivelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvpostodecombustivelApplication.class, args);
+	}
+
+}
